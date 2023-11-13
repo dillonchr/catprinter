59739a231ced75b9609a8d2574703674194b34f4
Dillon Christensen <j.dillon.christensen@gmail.com>
2023-11-12 22:06:54 -0600

    ready to go I think

--- test.py
+++ test.py
@@ -22,13 +22,15 @@ def create_text(text, font_name="IBMPlexMono-Regular.ttf", font_size=20):
     lines = []
     for line in text:
         lines.append(line)
-    height = len(lines) * 50
+    logo = PIL.Image.open("logo.png", "r")
+    height = len(lines) * 23 + logo.size[1]
     lines = "\n".join(lines)
 
     img = PIL.Image.new('RGB', (PrinterWidth, height), color = (255, 255, 255))
     font = PIL.ImageFont.truetype(font_name, font_size)
+    img.paste(logo, (0, 0))
     d = PIL.ImageDraw.Draw(img)
-    d.multiline_text((0,0), lines, fill=(0,0,0), font=font, spacing=-10)
+    d.multiline_text((0, logo.size[1]), lines, fill=(0,0,0), font=font, spacing=-10)
     trim(img)
     img.save("test.png")
 
