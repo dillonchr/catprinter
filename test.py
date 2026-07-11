cd2df66939fab894c5f62daa3cc1cd504af4483e
Dillon Christensen <seersuckerslacks@duck.com>
2026-07-10 21:42:06 -0500

    moved logo to be tiny so the received stamp works

--- test.py
+++ test.py
@@ -13,15 +13,15 @@ def create_text(text, font_name="IBMPlexMono-Regular.ttf", font_size=20):
     lines = []
     for line in text:
         lines.append(line)
-    logo = PIL.Image.open("logo.png", "r")
+    logo = PIL.Image.open("logo.png", "r").resize((142, 72))
     height = 30 + (len(lines) * 23 + logo.size[1])
     lines = "\n".join(lines)
 
     img = PIL.Image.new('RGB', (PrinterWidth, height), color = (255, 255, 255))
     font = PIL.ImageFont.truetype(font_name, font_size)
-    img.paste(logo, (0, 0))
+    img.paste(logo, (156, 71))
     d = PIL.ImageDraw.Draw(img)
-    d.multiline_text((0, logo.size[1]), lines, fill=(0,0,0), font=font, spacing=-10)
+    d.multiline_text((0, 177), lines, fill=(0,0,0), font=font, spacing=-10)
     #img.paste(img, (0, 30 + height // 2))
     img.save("test.png")
 
